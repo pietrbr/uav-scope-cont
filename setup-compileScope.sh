3ad92714c4d06e1dafb523d40c8cb9152e5f2fdd
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-06-28 18:38:56 -0400

    Update scope compiling script to copy modified files

--- setup-compileScope.sh
+++ setup-compileScope.sh
@@ -2,7 +2,7 @@
 
 # If option 'cp' is passed, the modified files of scope are copied from this folder into the scope folders: scope_api and scope_code
 if [[ $# -eq 0 ]];
-    then printf 'Remember: if you first want to copy the configuration files run again the command with the option 'compile':\n\tbash ./setup-compileScope.sh cp\n'
+    then printf 'Remember: if you first want to copy the edited scope files run again the command with the option 'compile':\n\tbash ./setup-compileScope.sh cp\n'
 elif [[ $# -eq 1 && $1 == 'cp' ]];
     then echo 'Copying modified scope file'
     cp ./scheduler_metric.cc /root/radio_code/srsLTE/srsenb/src/stack/mac/
