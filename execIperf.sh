92270a433ce47cd81af636c083de34742691fc62
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-06-28 17:19:10 -0400

    First commit

---
+++ execIperf.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd radio_code/srslte_config
+sleep 15
+ip=$(< ip.log grep 'The ip is: ' | cut -d':' -f2 | cut -d' ' -f2)
+echo $ip
+iperf3 -c $ip -u -b 0.5M
---
+++ ueRun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "add DNAT"
+iptables -t nat -A PREROUTING -d 172.16.0.8 -j DNAT --to-destination 240.84.80.1
+echo "Run server Iperf"
+tmux split-window "iperf3 -s"
+echo "Add IP route to Gnb"
+tmux split-window "bash addIPinterface.sh"
+echo "Run ue.config" 
+cd radio_code/srslte_config
+sudo srsue ue.conf
