5bbdb88bfd0ab7926690a41a09abbb5409795895
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-06-29 13:29:11 -0400

    Update setup script
    Now the copy of the cpp file of scope and the config file of tmux is separated.
    The script must be called twice with the two different options if you wanna copy them both

--- setup-compileScope.sh
+++ setup-compileScope.sh
@@ -2,13 +2,17 @@
 
 # If option 'cp' is passed, the modified files of scope are copied from this folder into the scope folders: scope_api and scope_code
 if [[ $# -eq 0 ]];
-    then printf 'Remember: if you first want to copy the edited scope files run again the command with the option "cp":\n\tbash ./setup-compileScope.sh cp\n\n'
+    then printf 'Remember: if you first want to copy the edited scope files  or the tmux configuration file run again the command with the options "cp" or "tm" like:\n\tbash ./setup-compileScope.sh cp\n\x\n\tbash ./setup-compileScope.sh tm\n\n'
 elif [[ $# -eq 1 && $1 == 'cp' ]];
-    then print 'fCopying modified scope file, and tmux configuration file\n\n'
+    then print 'fCopying modified scope file\n\n'
+    cp ./scheduler_metric.cc /root/radio_code/srsLTE/srsenb/src/stack/mac/
+	cp ./.tmux.conf ~/
+elif [[ $# -eq 1 && $1 == 'tm' ]];
+    then print 'fCopying tmux configuration file\n\n'
     cp ./scheduler_metric.cc /root/radio_code/srsLTE/srsenb/src/stack/mac/
 	cp ./.tmux.conf ~/
 elif [[ $# -eq 1 && $1 != 'cp' ]];
-    then printf 'No option like that... you wanted to write cp?\n\n'
+    then printf 'No option like that... you wanted to write cp or tm?\n\n'
 fi
 
 # Compile scope
