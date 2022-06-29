1e15eefefe0faf216aea42fdce324c9f30359890
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-06-29 17:44:00 -0400

    Update ueRun.sh to avoid iperf

--- ueRun.sh
+++ ueRun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "add DNAT"
 iptables -t nat -A PREROUTING -d 172.16.0.8 -j DNAT --to-destination 240.84.80.1
-echo "Run server Iperf"
+#echo "Run server Iperf"
 #tmux split-window "iperf3 -s"
 echo "Add IP route to Gnb"
 tmux split-window "bash addIPinterface.sh"
