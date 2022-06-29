d295f12390cc5c795a9926f291de8fb0905a44f0
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-06-29 13:57:10 -0400

    Update addIPinterface.sh
    Add IP addresses of Pietro's laptop. Still need to modify ueRun.sh

--- addIPinterface.sh
+++ addIPinterface.sh
@@ -5,7 +5,8 @@ while (($comm_ex!= "0"))
 do
         ip=$(ifconfig tun_srsue | grep 'inet addr:' | cut -d':' -f2 | cut -d' ' -f1)
 	echo $ip
-        ip route add 10.241.115.0/24 via $ip
+        #ip route add 10.241.115.0/24 via $ip
+        ip route add 10.244.105.0/24 via $ip
 	comm_ex=$(echo $?)
 	echo $comm_ex
 done
