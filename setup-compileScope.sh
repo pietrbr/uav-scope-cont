133e03f05e6f342c1cd4e4c63a5401e9bb5ea5fd
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-06-29 11:52:19 -0400

    Update setup-compileScope.sh to print better spacing

--- setup-compileScope.sh
+++ setup-compileScope.sh
@@ -2,13 +2,13 @@
 
 # If option 'cp' is passed, the modified files of scope are copied from this folder into the scope folders: scope_api and scope_code
 if [[ $# -eq 0 ]];
-    then printf 'Remember: if you first want to copy the edited scope files run again the command with the option 'compile':\n\tbash ./setup-compileScope.sh cp\n'
+    then printf 'Remember: if you first want to copy the edited scope files run again the command with the option "cp":\n\tbash ./setup-compileScope.sh cp\n\n'
 elif [[ $# -eq 1 && $1 == 'cp' ]];
-    then echo 'Copying modified scope file, and tmux configuration file'
+    then echo 'Copying modified scope file, and tmux configuration file\n/n'
     cp ./scheduler_metric.cc /root/radio_code/srsLTE/srsenb/src/stack/mac/
 	cp ./.tmux.conf ~/
 elif [[ $# -eq 1 && $1 != 'cp' ]];
-    then echo 'No option like that... you wanted to write cp?'
+    then echo 'No option like that... you wanted to write cp?\n\n'
 fi
 
 # Compile scope
