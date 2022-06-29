aeb6c5163a8f50d7cb05e877735d2bdda498fc6e
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-06-29 14:54:04 -0400

    Add DNAT config to epcRun.sh

--- epcRun.sh
+++ epcRun.sh
@@ -1,6 +1,8 @@
 #!/bin/bash  
-echo "Run enb.config"
+echo "add DNAT"
+iptables -t nat -A PREROUTING -d 240.84.80.1 -j DNAT --to-destination 172.16.0.8
 
+echo "Run enb.config"
 cd ~/radio_code/srslte_config
 
 sudo srsepc epc.conf
