fa16867ddb29d52019301f78e9ab779ebdd6a457
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-06-30 12:01:19 -0400

    Add comments for better readability

--- execIperf.sh
+++ execIperf.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd ~/radio_code/srslte_config
 sleep 15
+
+# This script first reads the IP address of the UE container from
+# the ip.log file, then starts iperf towards that ip address.
 ip=$(< ip.log grep 'The ip is: ' | cut -d':' -f2 | cut -d' ' -f2)
 echo $ip
 iperf3 -c $ip -u -b 0.5M
