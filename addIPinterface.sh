c714c83bd689025791f725149258d2cbd6814fff
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-06-30 12:01:19 -0400

    Add comments for better readability

--- addIPinterface.sh
+++ addIPinterface.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-echo "Run ue.config"
+echo "Looking for IP address of the BS host, rerouting packets through the bridge network for the way back"
 comm_ex="1"
 while (($comm_ex!= "0"))
 do
-        ip=$(ifconfig tun_srsue | grep 'inet addr:' | cut -d':' -f2 | cut -d' ' -f1)
+
+    ip=$(ifconfig tun_srsue | grep 'inet addr:' | cut -d':' -f2 | cut -d' ' -f1)
 	echo $ip
-        #ip route add 10.241.115.0/24 via $ip
-        ip route add 10.244.105.0/24 via $ip
+
+	# Matteo
+    #ip route add 10.241.115.0/24 via $ip
+	# Pietro
+    ip route add 10.244.105.0/24 via $ip
+
 	comm_ex=$(echo $?)
 	echo $comm_ex
 done
