7620791a839615864577fe4e939576c826dff5fc
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-07-07 19:54:45 -0400

    Add new ip addresses for setup renovation

--- ueRun.sh
+++ ueRun.sh
@@ -4,7 +4,8 @@ echo "add DNAT"
 # IP RULES
 # This DNAT configuration command reroutes traffic directed to 
 # the ue container to the ue host
-iptables -t nat -A PREROUTING -d 172.16.0.8 -j DNAT --to-destination 240.84.80.1
+iptables -t nat -A PREROUTING -d 172.16.0.8 -j DNAT --to-destination 10.76.105.1
+#iptables -t nat -A PREROUTING -d 172.16.0.8 -j DNAT --to-destination 240.84.80.1
 
 #echo "Run server Iperf"
 #tmux split-window "iperf3 -s"
