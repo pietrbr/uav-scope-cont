a75cf53ce808d2af19e332c5e7b9ae32888a0f6d
Pietro Brach del Prever <brachdelprever.p@northeastern.edu>
2022-07-07 19:54:45 -0400

    Add new ip addresses for setup renovation

--- epcRun.sh
+++ epcRun.sh
@@ -4,7 +4,8 @@ echo "add DNAT"
 # IP RULES
 # This DNAT configuration command should be always the same: it
 # reroutes traffic directed to ue host through the ue container
-iptables -t nat -A PREROUTING -d 240.84.80.1 -j DNAT --to-destination 172.16.0.8
+iptables -t nat -A PREROUTING -d 10.76.105.1 -j DNAT --to-destination 172.16.0.8
+#iptables -t nat -A PREROUTING -d 240.84.80.1 -j DNAT --to-destination 172.16.0.8
 
 echo "Run enb.config"
 cd ~/radio_code/srslte_config
